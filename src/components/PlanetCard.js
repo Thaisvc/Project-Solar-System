d0b0c54e84858a5e02b47d49908d993973d4dea0
thais <thaisviana081@outlook.com>
2022-06-21 16:15:51 -0300

    6. Renderize uma lista com os planetas do Sistema Solar

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -2,10 +2,9 @@ import React from 'react';
 
 class PlanetCard extends React.Component {
   render() {
-    const { name, image } = this.props;
     return (
       <div data-testid="planet-card">
-        <p data-testid="planet-name">{this.props.planetName}</p>
+        <span data-testid="planet-name">{this.props.planetName}</span>
         <img src={ this.props.planetImage } alt={ `Planeta ${this.props.planetName}` } />
       </div>
     );
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import Planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <section className="container-cards">
+          {
+            Planets.map((planetas, index) => (<PlanetCard
+              key={ index }
+              planetName={ planetas.name }
+              planetImage={ planetas.image }
+            />))
+          }
+        </section>
       </div>
-
     );
   }
 }
